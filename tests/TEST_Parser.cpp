97355fdbcae3e74fd7a3d2bf13c358c051faf225
Witold Lukasik <witek117@gmail.com>
2023-05-07 16:00:31 +0200

    Change parser
    Signed-off-by: Witold Lukasik <witek117@gmail.com>

--- tests/TEST_Parser.cpp
+++ tests/TEST_Parser.cpp
@@ -1,24 +1,24 @@
-#include <Command.hpp>
+#include <Parser.hpp>
 #include <gmock/gmock.h>
 
 TEST(PARSER, value_float) {
-    // const char *data0 = "12 5.763 344";
-    // auto[f, g, h] = parser::get<int, float, int>(data0);
-    // EXPECT_EQ(f, 12);
-    // EXPECT_EQ(g, (float) 5.763);
-    // EXPECT_EQ(h, 344);
+    auto d1 = parser::parse<double>("4.5324");
+    EXPECT_EQ(d1, 4.5324);
 
-    // const char *data1 = "2.345 4.235";
-    // auto[x1, y1] = parser::get<float, float>(data1);
-    // EXPECT_EQ(x1, (float) 2.345);
-    // EXPECT_EQ(y1, (float) 4.235);
+    auto d2 = parser::parse<int>("-1234");
+    EXPECT_EQ(d2, -1234);
 
-    // const char *data2 = "-234 5432";
-    // auto[x2, y2] = parser::get<int, int>(data2);
-    // EXPECT_EQ(x2, -234);
-    // EXPECT_EQ(y2, 5432);
+    auto d3 = parser::parse<uint32_t>("1234");
+    EXPECT_EQ(d3, 1234);
 
-    // const char *data3 = "2345";
-    // auto[x3] = parser::get<int>(data3);
-    // EXPECT_EQ(x3, 2345);
+    auto d4 = parser::parse<float>("-0.321");
+    EXPECT_EQ(d4, -0.321f);
+
+    auto d5 = parser::parse<char>("212");
+    EXPECT_EQ(d5, '2');
+
+    auto d6 = parser::parse<const char*>("Hello");
+    EXPECT_EQ(d6, "Hello");
+
+    parser::parse<void>("void");
 }
